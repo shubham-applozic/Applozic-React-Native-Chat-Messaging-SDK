80590376011aaa3141394ab1de3683d00a98b90d
Devashish Datt Mamgain <devashish@applozic.com>
2017-01-11 17:14:00 +0530

    #1 Initial project steup

---
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactActivity;
+import com.evollu.react.fcm.FIRMessagingPackage;
+import com.evollu.react.fcm.FIRMessagingPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "AwesomeProject";
+    }
+}
