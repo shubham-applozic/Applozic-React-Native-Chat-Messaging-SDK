a0d048b5f9e5357d3fbbf8f0fe4b944f3efa886c
adarshmishra <adarsh@applozic.com>
2018-01-19 20:18:58 +0530

    #initial commit

---
+++ ApplozicSample/chat.js
@@ -0,0 +1,267 @@
+//'use strict';
+import React, {Component} from 'react';
+//import FCM from 'react-native-fcm';
+
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    TextInput,
+    ScrollView,
+    NativeModules
+} from 'react-native';
+
+var ApplozicChat = NativeModules.ApplozicChat;
+
+export default class AwesomeProject extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: '',
+            email: '',
+            phoneNumer: '',
+            pass_word: '',
+            displayName: '',
+            loggedIn: false,
+            visible: false,
+            title: 'Login/SignUp',
+            mytoken: ''
+        };
+        this.openChat = this.openChat.bind(this);
+        this.isUserLogIn = this.isUserLogIn.bind(this);
+        this.chatLogin = this.chatLogin.bind(this);
+        this.initiateChat = this.initiateChat.bind(this);
+        this.logoutUser = this.logoutUser.bind(this);
+        this.show = this.show.bind(this);
+        this.tokenRefresh = this.tokenRefresh.bind(this);
+    }
+
+    componentDidMount() {
+
+        // FCM.getFCMToken().then(token => {
+        //     this.setState({mytoken: token});
+        //     console.log(token)
+        // });
+        //
+        // this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+        //     this.refreshToken()
+        //
+        // });
+
+      //  this.isUserLogIn();
+
+    }
+
+    componentWillUnMount()
+    {
+        this.refreshUnsubscribe();
+
+    }
+    //Login chat to the users..
+    chatLogin() {
+
+        if (this.state.userId.length > 0 && this.state.pass_word.length > 0) {
+          ApplozicChat.login({
+                'userId': this.state.userId,
+                'email': this.state.email,
+                'contactNumber': this.state.phoneNumber,
+                'password': this.state.pass_word,
+                'displayName': this.state.displayName
+            }, (error, response) => {
+              if(error){
+                  console.log(error)
+              }else{
+                this.setState({loggedIn: true, title: 'Loading...'}),
+                ApplozicChat.openChat(),
+                console.log(response)
+              }
+            })
+        } else {
+            this.setState({title: 'Login/SignUp'});
+            alert("Please Enter UserId & Password");
+        //ApplozicChat.login("test");
+
+         };
+    }
+
+
+  logoutUser() {
+        ApplozicChat.logoutUser({}, (response) => {
+            this.setState({
+                userId: '',
+                email: '',
+                phoneNumber: '',
+                pass_word: '',
+                displayName: '',
+                loggedIn: false,
+                title: 'Login/SignUp'
+            });
+        }, (error) => {
+            alert("logout error");
+        })
+    }
+
+    contactUnreadCount() {
+        ApplozicChat.contactUnreadCount({
+            'userId': 'ak01'
+        }, (response) => {
+            alert(response)
+        }, (error) => {
+            console.log(error)
+        },)
+    }
+
+    channelUnreadCount() {
+        ApplozicChat.channelUnreadCount({
+            'channelKey': '1234'
+        }, (response) => {
+            alert(response)
+        }, (error) => {
+            console.log(error)
+        },)
+    }
+
+    totalUnreadCount() {
+        ApplozicChat.totalUnreadCount({}, (response) => {
+            console.log(response)
+        }, (error) => {
+            console.log(error)
+        },)
+    }
+
+    isUserLogIn() {
+        ApplozicChat.isUserLogIn((response) => {
+            this.setState({loggedIn: response});
+        })
+    }
+
+    show() {
+        this.setState({title: 'Loading....!'});
+        this.chatLogin();
+    }
+
+    tokenRefresh()
+    {
+        ApplozicChat.tokenRefresh({
+            'token': this.state.mytoken
+        }, (response) => {
+            console.log(response)
+
+        }, (error) => {
+            console.log(error)
+        },)
+    }
+
+    render() {
+          let display = this.state.loggedIn;
+          if (display) {
+            return (
+			  <View style = {styles.container}>
+        <Text style = { styles.titleText} >
+            Applozic </Text>
+			  <Text style = {styles.baseText}>
+     			  Demo App </Text>
+			  <Text style = {styles.btn} onPress = {this.initiateChat}>
+				    Chat Logs </Text>
+			  <Text style = {styles.btn} onPress = {this.logoutUser}>
+     			  LogOut </Text>
+		     </View >
+            );
+        }
+
+        return (
+		    <View style ={styles.container}>
+            <ScrollView>
+              <Text style = {styles.titleText}>
+                 Applozic </Text>
+			        <Text style = {styles.baseText}>
+                 Demo App < /Text>
+              <TextInput style ={styles.inputText}
+                 keyboardType = "default"
+                 placeholder = "UserId"
+                 maxLength = {25}
+                 underlineColorAndroid = 'transparent'
+                 value = {this.state.userId}
+                 onChangeText={userId => this.setState({userId})}/>
+              <TextInput type = "email-address"
+                 style = {styles.inputText}
+                 placeholder = "Email"
+                 keyboardType = "email-address"
+                 maxLength = {30}
+                 underlineColorAndroid = 'transparent'
+                 value = { this.state.email}
+                 onChangeText = {email => this.setState({email})}/>
+              <TextInput style = { styles.inputText}
+                 placeholder = "Phone Number"
+                 keyboardType = "phone-pad"
+                 underlineColorAndroid = 'transparent'
+                 maxLength = {10}
+                 value = {this.state.phoneNumber}
+                 onChangeText = {phoneNumber => this.setState({phoneNumber})}/>
+              <TextInput id = "password"
+                 type = "password"
+                 style = {styles.inputText}
+                 maxLength = {25}
+                 placeholder = "Password"
+                 keyboardType = "default"
+                 underlineColorAndroid = 'transparent'
+                 value = {this.state.pass_word}
+                 secureTextEntry = {true}
+                 password = "true"
+                 onChangeText = {pass_word => this.setState({pass_word})}/>
+              <TextInput id = "displayName"
+                 style = {styles.inputText}
+                 placeholder = "Display Name"
+                 keyboardType = "default"
+                 underlineColorAndroid = 'transparent'
+                 value = {this.state.displayName}
+                 maxLength = {25}
+                 onChangeText = {displayName => this.setState({displayName})}/>
+              <Button title = {this.state.title}
+                 onPress = {this.show}
+                 color = "#20B2AA"/>
+              </ScrollView>
+			  </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#4D394B'
+    },
+    btn: {
+        fontSize: 23,
+        fontWeight: 'bold',
+        color: 'yellow',
+        marginTop: 20,
+        alignSelf: 'center'
+    },
+    baseText: {
+        fontFamily: 'Cochin',
+        color: '#fff',
+        marginBottom: 25,
+        alignSelf: 'center'
+    },
+    titleText: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginTop: 15,
+        alignSelf: 'center'
+    },
+    inputText: {
+        width: 330,
+        height: 40,
+        backgroundColor: '#fff',
+        marginBottom: 6,
+        padding: 10,
+        fontSize: 20,
+        marginLeft: 10,
+        marginRight: 10
+    }
+});
---
+++ ApplozicSample/ios/applozic/ApplozicChat.m
@@ -0,0 +1,149 @@
+//
+//  ApplozicChatManger.m
+//  ApplozicSample
+//
+//  Created by Adarsh on 17/01/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "ApplozicChat.h"
+#import "ALChatManager.h"
+#import <Applozic/ALUser.h>
+#import <Applozic/ALPushAssist.h>
+
+@implementation ApplozicChat
+
+// To export a module named CalendarManager
+RCT_EXPORT_MODULE();
+
+
+/**
+ * Login method of the user...
+ *
+ */
+
+RCT_EXPORT_METHOD(login:(NSDictionary *)userDetails  andCallback:(RCTResponseSenderBlock)callback )
+{
+  
+  ALUser * aluser =  [[ALUser alloc] initWithJSONString:[self getJsonString:userDetails]];
+  
+  ALChatManager * chatManger = [[ALChatManager alloc] init];
+  
+  [chatManger registerUserWithCompletion:aluser withHandler:^(ALRegistrationResponse *rResponse, NSError *error) {
+    
+    if(error){
+      
+      NSString* errorResponse = error.description;
+      if(rResponse){
+        
+        errorResponse = [self getJsonString:[rResponse dictionary]];
+      }
+      return callback(@[errorResponse, [NSNull null]]);
+
+    }else if ( rResponse.isRegisteredSuccessfully ){
+      
+      return callback(@[[NSNull null],[self getJsonString:[rResponse dictionary]]]);
+      
+    }
+  }];
+  
+  NSLog(@"Pretending to create an event  at ");
+  
+}
+/**
+ * Open chats
+ *
+ **/
+RCT_EXPORT_METHOD(openChat)
+{
+  
+  ALChatManager * chatManger = [[ALChatManager alloc] init];
+  ALPushAssist* pushAssistant = [[ALPushAssist alloc] init];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [chatManger launchChat:pushAssistant.topViewController];
+    
+  });
+
+}
+/**
+ * Open chat with Users
+ *
+ **/
+RCT_EXPORT_METHOD(openChatWithUser:(NSString*)userId)
+{
+  
+  ALChatManager * chatManger = [[ALChatManager alloc] init];
+  ALChatLauncher * chatLauncher = [[ALChatLauncher alloc] initWithApplicationId:chatManger.getApplicationKey];
+  ALPushAssist* pushAssistant = [[ALPushAssist alloc] init];
+
+  dispatch_async(dispatch_get_main_queue(), ^{
+    
+    [chatLauncher launchIndividualChat:userId withGroupId:nil andViewControllerObject:pushAssistant.topViewController andWithText:nil ];
+    
+  });
+  
+}
+/**
+ * Open chat with Group
+ *
+ **/
+RCT_EXPORT_METHOD(openChatWithGroup:(NSNumber*)groupId orClientGroupId:(NSString*) clientGroupId)
+{
+  
+  ALChatManager * chatManger = [[ALChatManager alloc] init];
+  ALChatLauncher * chatLauncher = [[ALChatLauncher alloc] initWithApplicationId:chatManger.getApplicationKey];
+  ALPushAssist* pushAssistant = [[ALPushAssist alloc] init];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    
+    [chatLauncher launchIndividualChat:nil withGroupId:groupId andViewControllerObject:pushAssistant.topViewController andWithText:nil ];
+    
+  });
+}
+
+/**
+ *  Logout users
+ *
+ */
+
+
+RCT_EXPORT_METHOD(logoutUser: andCallback:(RCTResponseSenderBlock)callback )
+{
+  ALRegisterUserClientService * alRegisterUserClientService = [[ALRegisterUserClientService alloc]init];
+  
+  [alRegisterUserClientService logoutWithCompletionHandler:^(ALAPIResponse *response, NSError *error) {
+    if(error){
+      
+      NSString* errorResponse = error.description;
+      return callback(@[errorResponse, [NSNull null]]);
+      
+    }else if (response ){
+      
+      return callback(@[[NSNull null],[self getJsonString:[response dictionary]]]);
+      
+    }
+  }];
+  
+}
+
+
+-(NSString *)getJsonString:(id) Object{
+
+  NSError *error;
+  NSString *jsonString;
+  NSData *jsonData = [NSJSONSerialization dataWithJSONObject:Object
+                                                     options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                       error:&error];
+  
+  if (! jsonData) {
+    
+    NSLog(@"Got an error: %@", error);
+    
+  } else {
+    
+    jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+  }
+  return jsonString;
+}
+                        
+
+@end
